1f31e02a0c4043cd27338d8e107b6da2429534f9
kburk1997 <kburk97@optonline.net>
2017-03-15 13:16:31 -0400

    generate API keys

--- stemfest_api/services/apps.py
+++ stemfest_api/services/apps.py
@@ -1,7 +1,13 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
+from django.db.models import signals
+from tastypie.models import create_api_key
 
 class ServicesConfig(AppConfig):
     name = 'services'
+
+    def ready(self):
+       # This line dispatches signal to Tastypie to create APIKey
+       signals.post_save.connect(create_api_key, sender=User)
