ee5cba860df90e26e242374ed3f15b934455b52d
kburk1997 <kburk97@optonline.net>
2017-03-15 12:53:31 -0400

    add activity model

--- stemfest_api/services/models.py
+++ stemfest_api/services/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Activity(models.Model):
+  name=models.CharField(max_length=30)
+  presenter=models.CharField(max_length=100)
+  grades=models.CharField(max_length=10)
+  description=models.TextField()
+
+  def __str__(self):
+    return self.name
